f3a646c77be7a74187a0301832103f892ee6ef66
Nina Rubesa <nina.rubesa@gmail.com>
2024-03-29 14:06:47 +0100

    coloring of login and signup

--- src/components/views/Register.tsx
+++ src/components/views/Register.tsx
@@ -81,8 +81,8 @@ const Register = () => {
   return (
     <BaseContainer>
       <div className="register container">
-        <h1>Register</h1>
-        <p>Use unique username and name</p>
+        <h1 style={{ color: '#201d18' }}>Register</h1>
+        <p style={{ color: '#201d18' }}>Use unique username and name</p>
         <div className="register form">
           <FormField
             label="Username"
