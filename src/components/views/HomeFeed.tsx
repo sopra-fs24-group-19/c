bf2aafbb56b8c46a56913499bd342cc58660789a
Nina Rubesa <nina.rubesa@gmail.com>
2024-03-29 12:01:01 +0100

    before changing -theme.scss

--- src/components/views/HomeFeed.tsx
+++ src/components/views/HomeFeed.tsx
@@ -1,12 +1,11 @@
 import Header from 'components/ui/Header';
 
-
 const HomeFeed = () => {
   return (
     <>
-        <Header />
-        <h2>Home Feed Page</h2>
-        <p>This is a placeholder for the Home Feed page content.</p>
+      <Header />
+        <h2>HomeFeed Page</h2>
+        <p>This is a placeholder for the HomeFeed page content.</p>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: {
+          500: '#f5f5dc',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
